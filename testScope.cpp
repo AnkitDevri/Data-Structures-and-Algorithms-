e100d3f5009e7025801a6b35f54c5a0e9abc3b46
AnkitDevri <53830927+AnkitDevri@users.noreply.github.com>
2021-12-17 09:51:43 +0530

    recursive digit sum edit

--- testScope.cpp
+++ testScope.cpp
@@ -1,14 +1,33 @@
 using namespace std;
 #include<bits/stdc++.h>
-
-int func(){
-vector<int> arr(100,0);
-for(auto i : arr) cout<<arr[i]<<endl;
+void recDigitSum(string& number){
+    if(number.size()==1){
+        return;
+    }
+    int num = 0;
+    for(auto i : number){
+        num += i -'0';
+    }
+    number = to_string(num);
+    recDigitSum(number);
 }
-
 int main(){
-    for(int i=0;i<100000;i++)
-     func();
-    getchar();
+    #ifndef ONLINE_JUDGE
+    freopen("D:\\input.txt","r",stdin);
+    freopen("D:\\output.txt","w",stdout);
+    #endif
+    
+    string num;
+    int k;
+    cin>>num;
+    cin>>k;
+    int number = 0;
+    for(int i = 0; i < num.size(); i++){
+        number+= num[i]-'0';
+    }
+    number*=k;
+    num = to_string(number);
+    recDigitSum(num);
+    cout<<num;
     return 0;
 }
