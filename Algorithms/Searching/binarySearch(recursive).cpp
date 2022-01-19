736a78865654ad33043910a7ae16d50a385d4bfc
AnkitDevri <53830927+AnkitDevri@users.noreply.github.com>
2022-01-19 11:53:25 +0530

    search Rotated new code

--- Algorithms/Searching/binarySearch(recursive).cpp
+++ Algorithms/Searching/binarySearch(recursive).cpp
@@ -2,19 +2,11 @@ using namespace std;
 #include<bits/stdc++.h>
  int search(vector<int>& data,int left,int right,int& element){
    int mid=(left+right)/2;
-   if(left>right)
-    return -1;
-    else{
-    if(left==right && data[mid]!=element)
-     return -1;
-    if(data[mid]==element)
-    return mid;
-    else if(element>data[mid])
-    return search(data,mid+1,right,element);
-    else
-    return search(data,left,mid,element);
-    }
-
+   if(left>right) return -1;
+   if(left==right && data[mid]!=element) return -1;
+    if(data[mid]==element) return mid;
+    else if(element>data[mid]) return search(data,mid+1,right,element);
+    else return search(data,left,mid,element);
  }
 int main(){
     int n,ele;
@@ -26,7 +18,7 @@ int main(){
     }
     int element;
     cin>>element;
-
+    
     int ans= search(data,0,n-1,element); 
        if(ans==-1){
         cout<<"\nNot found";
